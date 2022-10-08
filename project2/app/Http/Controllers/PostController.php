1972191414f92c50b9c49368e5055b3f2046537c
ismaelahmad6333 <106573961+ismaelahmad6333@users.noreply.github.com>
2022-10-08 09:00:10 +0100

    Added postcontroller

--- project2/app/Http/Controllers/PostController.php
+++ project2/app/Http/Controllers/PostController.php
@@ -15,7 +15,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::get();
 
         return view('blog.index');
     }
